cf1264c3a4dea722d1a46b60a6b040d411acb5ba
BENE Tom <tom.bene@telecomnancy.eu>
2021-12-01 13:51:03 +0100

    Moved files around for clarity

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask
-from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template, request
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        form = request.form
     return render_template('login.html')
